c86965e7dd0d6460ceafd7e507f193f32b787aa3
Kimmi Gan <kimmi.gan@onfido.com>
2020-05-18 12:04:14 +0100

    Add react-router-dom for basic routing with BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,4 +1,10 @@
 import React, { Component } from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 import logo from "./logo.svg";
 import "./App.css";
 
@@ -15,15 +21,36 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <OnfidoLoader
-            locale={this.state.locale}
-            person={this.state.applicant}
-            updatePerson={this.submitCheckOnComplete} />
-        </header>
-      </div>
+      <Router>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h2>Onfido React Sample App</h2>
+            <nav>
+              <ul className="App-links">
+                <li className="App-link"><Link to="/">Home</Link></li>
+                <li className="App-link"><Link to="/identity">Identity Check</Link></li>
+                <li className="App-link"><Link to="/about">About</Link></li>
+              </ul>
+            </nav>
+
+            <Switch>
+              <Route path="/about">
+                <About />
+              </Route>
+              <Route path="/identity">
+                <OnfidoLoader
+                  locale={this.state.locale}
+                  person={this.state.applicant}
+                  updatePerson={this.submitCheckOnComplete} />
+              </Route>
+              <Route path="/">
+                <Home />
+              </Route>
+            </Switch>
+          </header>
+        </div>
+      </Router>
     );
   }
 
@@ -33,5 +60,9 @@ class App extends Component {
   }
 
 }
+
+const Home = () => <div><h2>Home</h2><p>Content for Home page</p></div>;
+const About = () => <div><h2>About</h2><p>Content for About page</p></div>;
+// const Identity = () => <div><h2>Identity Check</h2><p>Page with Onfido SDK</p></div>;
 
 export default App;
