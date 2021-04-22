0e74ca5668484696a0f1863ed8aff5b08623544a
Kimmi Gan <kimmi.gan@onfido.com>
2021-04-22 09:42:14 +0100

    Remove CRA default header UI

--- src/App.js
+++ src/App.js
@@ -5,18 +5,13 @@ import "./App.css";
 import OnfidoLoader from "./OnfidoLoader.js";
 
 class App extends Component {
-
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <OnfidoLoader />
-        </header>
+        <OnfidoLoader />
       </div>
     );
   }
-
 }
 
 export default App;
